974e083ed02b62812a838413214b2a5eec5f5d1f
yuequan <yuequanaaaaa@outlook.com>
2017-10-27 19:42:17 +0800

    动态权限申请

--- app/src/main/java/buzz/pushfit/im/mvp/view/activity/LoginActivity.kt
+++ app/src/main/java/buzz/pushfit/im/mvp/view/activity/LoginActivity.kt
@@ -1,6 +1,9 @@
 package buzz.pushfit.im.mvp.view.activity
 
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
 import buzz.pushfit.im.R
 import buzz.pushfit.im.base.BaseActivity
 import buzz.pushfit.im.mvp.presenter.impl.LoginPresenter
@@ -33,9 +36,30 @@ class LoginActivity : BaseActivity(), ILoginView {
     fun login() {
         //隐藏软件盘
         hideSoftKeyboard()
+        if (hasWriteExternalStoragePermission()){
         val username = username.text.trim().toString()
         val password = password.text.trim().toString()
         mPresenter.onLogin(username, password)
+        } else applyWriteExternalStoragePermission()
+    }
+
+    //申请写磁盘权限
+    private fun applyWriteExternalStoragePermission() {
+        var permissions= arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ActivityCompat.requestPermissions(this,permissions,0)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            login()
+        }else toast(getString(R.string.permission_reject))
+    }
+
+
+    //判断是否有写磁盘权限
+    private fun hasWriteExternalStoragePermission(): Boolean {
+        var result = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        return result==PackageManager.PERMISSION_GRANTED
     }
 
     override fun onUserNameError() {
@@ -59,7 +83,7 @@ class LoginActivity : BaseActivity(), ILoginView {
         finish()
     }
 
-    override fun onLoginFailed(code:Int,message:String) {
+    override fun onLoginFailed(code: Int, message: String) {
         //隐藏进度条
         dismissProgress()
         toast(message)
