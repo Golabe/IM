2bd5301b641dcce65f20879e996e80ba40689938
yuequan <yuequanaaaaa@outlook.com>
2017-10-28 21:44:50 +0800

    聊天界面MVP

--- app/src/main/java/buzz/pushfit/im/mvp/presenter/impl/ChatPresenter.kt
+++ app/src/main/java/buzz/pushfit/im/mvp/presenter/impl/ChatPresenter.kt
@@ -1,13 +1,15 @@
 package buzz.pushfit.im.mvp.presenter.impl
 
 import buzz.pushfit.im.mvp.presenter.IChatPresenter
+import buzz.pushfit.im.mvp.view.IChatView
 
 /**
  * Created by yuequan on 2017/10/28.
  */
-class ChatPresenter:IChatPresenter {
-
-    override fun onSendMessage(message: String) {
+class ChatPresenter(val view:IChatView):IChatPresenter {
+    override fun onSendMessage(contact: String, message: String) {
 
     }
+
+
 }
--- app/src/main/java/buzz/pushfit/im/mvp/view/activity/ChatActivity.kt
+++ app/src/main/java/buzz/pushfit/im/mvp/view/activity/ChatActivity.kt
@@ -1,42 +1,91 @@
 package buzz.pushfit.im.mvp.view.activity
 
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.text.Editable
-import android.text.TextWatcher
+import android.view.KeyEvent
 import android.view.View
+import android.widget.TextView
 import buzz.pushfit.im.R
 import buzz.pushfit.im.adapter.TextWatcherAdapter
 import buzz.pushfit.im.base.BaseActivity
+import buzz.pushfit.im.mvp.presenter.impl.ChatPresenter
+import buzz.pushfit.im.mvp.view.IChatView
 import kotlinx.android.synthetic.main.abc_activity_chat.*
 import kotlinx.android.synthetic.main.layout_header.*
+import org.jetbrains.anko.toast
 
-class ChatActivity : BaseActivity() {
+class ChatActivity : BaseActivity(), IChatView {
 
+    val presenter = ChatPresenter(this)
+    lateinit var mRecyclerView: RecyclerView
+    lateinit var username: String
 
     override fun getLayoutResId(): Int = R.layout.abc_activity_chat
 
     override fun init() {
         super.init()
+
         initHeader()
         initEdit()
+        initRecyclerView()
+
+        send.setOnClickListener {
+            sendMessage()
+
+        }
+
+        edit.setOnEditorActionListener { _, _, _ ->
+            sendMessage()
+            true
+        }
+    }
+
+    private fun sendMessage() {
+        hideSoftKeyboard()
+        val message = edit.text.trim().toString()
+        presenter.onSendMessage(username, message)
+    }
+
+    private fun initRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerView)
+        mRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+
+        }
     }
 
     private fun initEdit() {
         edit.addTextChangedListener(object : TextWatcherAdapter() {
             override fun afterTextChanged(p0: Editable?) {
-                send.isEnabled=!p0.isNullOrEmpty()
+                send.isEnabled = !p0.isNullOrEmpty()
             }
         })
     }
 
     private fun initHeader() {
-        val username= intent.getStringExtra("username")
-        headerLeft.visibility=View.VISIBLE
+        username = intent.getStringExtra("username")
+        headerLeft.visibility = View.VISIBLE
         headerLeft.setImageResource(R.mipmap.ic_back_48)
         headerLeft.setOnClickListener { finish() }
-        val title= String.format(getString(R.string.chating,username))
-        headerTitle.text=title
+        val title = String.format(getString(R.string.chating, username))
+        headerTitle.text = title
 
     }
 
+    override fun onStartSendMessage() {
+        mRecyclerView.adapter.notifyDataSetChanged()
+    }
+
+    override fun onSendMessageSuccess() {
+        mRecyclerView.adapter.notifyDataSetChanged()
+        toast(getString(R.string.send_message_success))
+        edit.text.clear()//清除编辑框
+    }
+
+    override fun onSendMessageFailed() {
+        toast(getString(R.string.send_message_failed))
+        mRecyclerView.adapter.notifyDataSetChanged()
+    }
 
 }
