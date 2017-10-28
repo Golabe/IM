edad9257b1046a8ac10cd8210d111675c58339bb
yuequan <yuequanaaaaa@outlook.com>
2017-10-28 20:55:57 +0800

    收发布局Item

---
+++ app/src/main/java/buzz/pushfit/im/widget/SendMessageItemView.kt
@@ -0,0 +1,16 @@
+package buzz.pushfit.im.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RelativeLayout
+import buzz.pushfit.im.R
+
+/**
+ * Created by yuequan on 2017/10/28.
+ */
+class SendMessageItemView(context: Context?, attrs: AttributeSet?=null) : RelativeLayout(context, attrs) {
+    init {
+        View.inflate(context, R.layout.item_send_message_view,this)
+    }
+}
