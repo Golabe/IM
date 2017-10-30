4c82bcd3f6576ca5803bbe7cafeed8e4cd6918c1
yuequan <yuequanaaaaa@outlook.com>
2017-10-30 19:04:23 +0800

    后台收到消息弹出Notifitcation

--- app/src/main/java/buzz/pushfit/im/app/IMApplication.kt
+++ app/src/main/java/buzz/pushfit/im/app/IMApplication.kt
@@ -2,13 +2,18 @@ package buzz.pushfit.im.app
 
 import android.app.ActivityManager
 import android.app.Application
+import android.app.Notification
+import android.app.NotificationManager
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.media.AudioManager
+import android.media.SoundPool
 import buzz.pushfit.im.BuildConfig
+import buzz.pushfit.im.R
 import buzz.pushfit.im.adapter.EMMessageListenerAdapter
 import cn.bmob.v3.Bmob
-import com.hyphenate.chat.EMClient
-import com.hyphenate.chat.EMMessage
-import com.hyphenate.chat.EMOptions
+import com.hyphenate.chat.*
+import org.jetbrains.anko.doAsync
 
 /**
  * Created by yuequan on 2017/10/27.
@@ -18,15 +23,28 @@ class IMApplication : Application() {
     companion object {
         lateinit var instance: IMApplication
     }
-    val messageListener=object : EMMessageListenerAdapter() {
+
+    val soundPool = SoundPool(2, AudioManager.STREAM_MUSIC, 0)
+    val shortMusic by lazy {
+        soundPool.load(instance, R.raw.shortmusic, 0)
+    }
+    val longMusic by lazy {
+        soundPool.load(instance, R.raw.longmusic, 0)
+    }
+    private val messageListener = object : EMMessageListenerAdapter() {
         override fun onMessageReceived(messages: List<EMMessage>) {
             super.onMessageReceived(messages)
-            //前台播放短音
-            //后台播放长音
+            if (isForeground()) {
+                //如果在前台播放短音
+                soundPool.play(longMusic, 1f, 1f, 0, 0, 1f)
+            } else {
+                //如果在后台播放长音
+                soundPool.play(shortMusic, 1f, 1f, 0, 0, 1f)
+                showNotification(messages)
 
+            }
         }
     }
-
     override fun onCreate() {
         super.onCreate()
         instance = this
@@ -37,14 +55,37 @@ class IMApplication : Application() {
         Bmob.initialize(this, "e452cb800a33ff9a25aa99035dbc7df5")
         EMClient.getInstance().chatManager().addMessageListener(messageListener)
     }
+    private fun showNotification(messages: List<EMMessage>?) {
+        val notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-    private fun isForeground():Boolean{
+        var contentText = getString(R.string.no_text_message)
+        messages?.forEach {
+            if (it.type == EMMessage.Type.TXT) {
+                contentText = (it.body as EMTextMessageBody).message
+            }
+            val notification = Notification.Builder(this)
+                    .setContentTitle(getString(R.string.have_new_message))
+                    .setContentText(contentText)
+                    .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+                    .setSmallIcon(R.mipmap.ic_launcher)
+
+                    .notification
+            notificationManager.notify(0,notification)
+
+
+        }
+    }
+
+
+
+    private fun isForeground(): Boolean {
         val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         activityManager.runningAppProcesses
-                .filter { it.processName==packageName }
-                .forEach { //找到App进程
-                    return    it.importance==ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+                .filter { it.processName == packageName }
+                .forEach {
+                    //找到App进程
+                    return it.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
                 }
-        return false
+        return true
     }
 }
